6330e756f228117d8e3c5cf1e387bedbd351257e
MedHedii <79151270+MedHedii@users.noreply.github.com>
2021-05-20 15:20:52 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+const connectDB = require("./config/connectDB");
+
+const userRoute = require("./routes/userRoutes");
+
+app.use(express.json());
+
+connectDB();
+
+
+app.use("/models/User", userRoute);
+const port = process.env.PORT;
+app.listen(port, err => {
+    err ? console.log("error") : console.log('server is running on server${port}')}
+    );
